80e26dd5b3bd0cda5f13a59ebacc0a340033057a
leeweb3 <mw5130558@gmail.com>
2023-12-10 19:31:20 +0800

    feat: my tasks

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,183 +1,180 @@
-
 // 身份
 export enum Identification {
-	VISITOR = 99,
-	ENGINEER = 1,
-	OPERATOR = 2
+  VISITOR = 99,
+  ENGINEER = 1,
+  OPERATOR = 2,
 }
 
 export enum IPath {
-	TASKS=`task`,
-	MYREQUIREMENT=`myrequirement`,
-	MYTASKS=`mytasks`
+  TASKS = `tasks`,
+  MYREQUIREMENT = `myrequirement`,
+  MYTASKS = `mytasks`,
 }
 
 export const enum TabsEnum {
-	SINGLE_TASK= '单一任务',
-	PERSON_TASK= '人员需求',
-	ALL_TASK = '整包项目'
+  SINGLE_TASK = '单一任务',
+  PERSON_TASK = '人员需求',
+  ALL_TASK = '整包项目',
 }
 export enum RequirementType {
-  Single=1,
-  Person=2,
-  Global=3
+  Single = 1,
+  Person = 2,
+  Global = 3,
 }
 // 需求类型
 export const Tabs = [
-	{
-		label: TabsEnum.SINGLE_TASK,
-		value: RequirementType.Single
-	},
-	{
-		label: TabsEnum.PERSON_TASK,
-		value: RequirementType.Person
-	},
-	{
-		label: TabsEnum.ALL_TASK,
-		value: RequirementType.Global
-	},
-]
+  {
+    label: TabsEnum.SINGLE_TASK,
+    value: RequirementType.Single,
+  },
+  {
+    label: TabsEnum.PERSON_TASK,
+    value: RequirementType.Person,
+  },
+  {
+    label: TabsEnum.ALL_TASK,
+    value: RequirementType.Global,
+  },
+];
 
 export enum ProType {
-	BIDDING="1",
-	PITCH="2",
-	BID="3"
+  BIDDING = '1',
+  PITCH = '2',
+  BID = '3',
 }
 
 // 众包方式
 export const ProTypes = [
-	{
-		label: '竞标',
-		value: ProType.BIDDING
-	},
-	{
-		label: '比稿',
-		value: ProType.PITCH
-	},
-	{
-		label: '出价',
-		value: ProType.BID
-	}
-]
+  {
+    label: '竞标',
+    value: ProType.BIDDING,
+  },
+  {
+    label: '比稿',
+    value: ProType.PITCH,
+  },
+  {
+    label: '出价',
+    value: ProType.BID,
+  },
+];
 // 任务类型
 export const TaskTypes = [
-	{
-		label: '普通任务',
-		value: '1'
-	},
-	{
-		label: '船长任务',
-		value: '2'
-	}
-]
+  {
+    label: '普通任务',
+    value: '1',
+  },
+  {
+    label: '船长任务',
+    value: '2',
+  },
+];
 
 export enum IStatus {
-	CLOSED='1',  //已关闭
-	EVALUATION='2', // 评估中
-	WAIT_SIGN='3', // 待签约
-	SIGNED= '4', // 已签约
-	CODEING='5', // 进行中
-	WAIT_ACCEPT='6', //待验收
-	COMPLETE= '7', // 已完成
-	UN_BID='8' // 未中标
-} 
+  CLOSED = '1', //已关闭
+  EVALUATION = '2', // 评估中
+  WAIT_SIGN = '3', // 待签约
+  SIGNED = '4', // 已签约
+  CODEING = '5', // 进行中
+  WAIT_ACCEPT = '6', //待验收
+  COMPLETE = '7', // 已完成
+  UN_BID = '8', // 未中标
+}
 
 export const RequirementStatus = [
-	{
-		label: '已关闭',
-		value: IStatus.CLOSED
-	},
-	{
-		label: '评估中',
-		value: IStatus.EVALUATION
-	},
-	{
-		label: '待签约',
-		value: IStatus.WAIT_SIGN
-	},
-	{
-		label: '已签约',
-		value: IStatus.SIGNED
-	},
-	{
-		label: '进行中',
-		value: IStatus.CODEING
-	},
-	{
-		label: '待验收',
-		value: IStatus.WAIT_ACCEPT
-	},
-	{
-		label: '已完成',
-		value: IStatus.COMPLETE
-	}
-]
+  {
+    label: '已关闭',
+    value: IStatus.CLOSED,
+  },
+  {
+    label: '评估中',
+    value: IStatus.EVALUATION,
+  },
+  {
+    label: '待签约',
+    value: IStatus.WAIT_SIGN,
+  },
+  {
+    label: '已签约',
+    value: IStatus.SIGNED,
+  },
+  {
+    label: '进行中',
+    value: IStatus.CODEING,
+  },
+  {
+    label: '待验收',
+    value: IStatus.WAIT_ACCEPT,
+  },
+  {
+    label: '已完成',
+    value: IStatus.COMPLETE,
+  },
+];
 
 export const TaskStatus = [
-	{
-		label: '待签约',
-		value: IStatus.WAIT_SIGN
-	},
-	{
-		label: '未中标',
-		value: IStatus.UN_BID
-	},
-	{
-		label: '已签约',
-		value: IStatus.SIGNED
-	},
-	{
-		label: '进行中',
-		value: IStatus.CODEING
-	},
-	{
-		label: '待验收',
-		value: IStatus.WAIT_ACCEPT
-	},
-	{
-		label: '已完成',
-		value: IStatus.COMPLETE
-	}
-]
-
+  {
+    label: '待签约',
+    value: IStatus.WAIT_SIGN,
+  },
+  {
+    label: '未中标',
+    value: IStatus.UN_BID,
+  },
+  {
+    label: '已签约',
+    value: IStatus.SIGNED,
+  },
+  {
+    label: '进行中',
+    value: IStatus.CODEING,
+  },
+  {
+    label: '待验收',
+    value: IStatus.WAIT_ACCEPT,
+  },
+  {
+    label: '已完成',
+    value: IStatus.COMPLETE,
+  },
+];
 
 // 职位类型
 export const ProfessionTypes = [
-	{
-		label: '前端开发',
-		value: '1'
-	},
-	{
-		label: '后端开发',
-		value: '2'
-	},
-	{
-		label: 'UI设计',
-		value: '3'
-	},
-	{
-		label: '测试',
-		value: '4'
-	},
-]
+  {
+    label: '前端开发',
+    value: '1',
+  },
+  {
+    label: '后端开发',
+    value: '2',
+  },
+  {
+    label: 'UI设计',
+    value: '3',
+  },
+  {
+    label: '测试',
+    value: '4',
+  },
+];
 
 export interface IRequirement {
-  projectName: string
-  description: string
-  categoryId: number
-  fileList: string[]
-  requirementList: string[]
-  contactInfo: string
+  projectName: string;
+  description: string;
+  categoryId: number;
+  fileList: string[];
+  requirementList: string[];
+  contactInfo: string;
 }
 
-export interface IRequirementSingle extends IRequirement{
-	crowSourcingMethod: number
-	professionType: number
+export interface IRequirementSingle extends IRequirement {
+  crowSourcingMethod: number;
+  professionType: number;
 }
 export interface IRequirementPerson extends IRequirement {
-	educational: string
-	experience: string
-	workPlace: string
-	workTime: string
-
+  educational: string;
+  experience: string;
+  workPlace: string;
+  workTime: string;
 }
--- src/views/task/Template.tsx
+++ src/views/task/Template.tsx
@@ -1,75 +1,84 @@
 // import { Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react"
-import { Box, Flex, Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react";
-import {  useSingleTaskFilter, useTaskList, useTasks } from "hooks/task";
+import { Box, Flex, Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react';
+import { useSingleTaskFilter, useTaskList, useTasks } from 'hooks/task';
 
-import AdminLayout from "layouts/admin";
-import { useEffect } from "react";
-import { RequirementType, TabsEnum } from "utils/constant";
-import styles from './index.module.scss'
-import PersonTask from "./list/PersonTask";
-import SingleTask from "./list/SingleTask";
+import AdminLayout from 'layouts/admin';
+import { useEffect } from 'react';
+import { RequirementType, TabsEnum } from 'utils/constant';
+import styles from './index.module.scss';
+import PersonTask from './list/PersonTask';
+import SingleTask from './list/SingleTask';
 
-export default function TaskTemplate (props: {
-	children?: React.ReactNode,
-	isMine?: boolean // 是否是我的
-	from?: string // 来自我的需求、大厅、我的任务
-  tabs: any[],
-  activeTab: RequirementType,
-  handleTabChange: (val:RequirementType) => void,
-  data: {loading:boolean, data:any[], hasMore: boolean, fetchMoreData: ()=> void, refetchData: () => void, handleSearch: (val:string) => void, onChange: (val:string, s: string) => void,refreshFilter:() => void},
+export default function TaskTemplate(props: {
+  children?: React.ReactNode;
+  isMine?: boolean; // 是否是我的
+  from?: string; // 来自我的需求、大厅、我的任务
+  tabs: any[];
+  activeTab: RequirementType;
+  handleTabChange: (val: RequirementType) => void;
+  data: {
+    loading: boolean;
+    data: any[];
+    hasMore: boolean;
+    fetchMoreData: () => void;
+    refetchData: () => void;
+    handleSearch: (val: string) => void;
+    onChange: (val: string, s: string) => void;
+    refreshFilter: () => void;
+  };
   // person: {loading:boolean, data:any[], hasMore: boolean, fetchMoreData: ()=> void, refetchData: () => void, handleSearch: (val:string) => void, onChange: (val:string, s: string) => void,refreshFilter:() => void}
-}){
+}) {
   const {
     tabs,
     activeTab,
     handleTabChange,
     data,
-		children
+    children,
     // person
-  } = props    
+  } = props;
   useEffect(() => {
     // 单一任务
-    data.refreshFilter()
-    data.refetchData()
-  }, [activeTab])
+    data.refreshFilter();
+    data.refetchData();
+  }, [activeTab]);
 
-  
   // // 搜索任务
   // const handleSearch = (searchVal: string) => {
   //   console.log("searchval", searchVal)
   //   onChange('name', searchVal)
   // }
   return (
-		<AdminLayout>
-			<Box pt={{ base: '130px', md: '80px', xl: '80px' }} position="relative">
-				<Tabs variant='soft-rounded' colorScheme="purple">
-					<Box className={styles.tabswrap} > 
-						<TabList className={styles.tabs}>
-							{
-								tabs?.map(it => {
-									return (
-										<Tab key={it.label} onClick={() => handleTabChange(it.value)}>{it.label}</Tab>
-									)
-								})
-							}
-						</TabList>
-					</Box>
-					<TabPanels padding="0">
-						{
-							tabs?.map(it => {
-								return (
-									<TabPanel padding="0" key={`${it.label}_tabcontent`}>
-										{it.label === TabsEnum.SINGLE_TASK && <SingleTask single={data} isMine={props.isMine} from={props.from}/>}
-										{it.label === TabsEnum.PERSON_TASK && <PersonTask person={data} isMine={props.isMine} from={props.from}/>}
-									</TabPanel>
-								)
-							})
-						}
-					</TabPanels>
-				</Tabs>
-				{children && <Box className={styles.add}>{children}</Box>}
-			</Box>
-		</AdminLayout>
-	)
+    <AdminLayout>
+      <Box pt={{ base: '130px', md: '80px', xl: '80px' }} position="relative">
+        <Tabs variant="soft-rounded" colorScheme="purple">
+          <Box className={styles.tabswrap}>
+            <TabList className={styles.tabs}>
+              {tabs?.map((it) => {
+                return (
+                  <Tab key={it.label} onClick={() => handleTabChange(it.value)}>
+                    {it.label}
+                  </Tab>
+                );
+              })}
+            </TabList>
+          </Box>
+          <TabPanels padding="0">
+            {tabs?.map((it) => {
+              return (
+                <TabPanel padding="0" key={`${it?.label}_tabcontent`}>
+                  {it.label === TabsEnum.SINGLE_TASK && (
+                    <SingleTask single={data} isMine={props.isMine} from={props.from} />
+                  )}
+                  {it.label === TabsEnum.PERSON_TASK && (
+                    <PersonTask person={data} isMine={props.isMine} from={props.from} />
+                  )}
+                </TabPanel>
+              );
+            })}
+          </TabPanels>
+        </Tabs>
+        {children && <Box className={styles.add}>{children}</Box>}
+      </Box>
+    </AdminLayout>
+  );
 }
-
