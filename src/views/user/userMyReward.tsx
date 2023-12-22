86f3a4ebaf5ea3c5c77d790ea902ada780d21802
bella <bellawmm69@gmail.com>
2023-12-22 10:57:09 +0800

    feat: 1

---
+++ src/views/user/userMyReward.tsx
@@ -0,0 +1,49 @@
+import { Box, Text, Flex,Image, Tag } from "@chakra-ui/react";
+import styles from './index.module.scss'
+
+export default function UserMyReward(props: {
+  data: any[]
+}) {
+  const {data} = props
+  return <Box
+    display="flex"
+    flexDirection="column"
+    alignItems="flex-start"
+    justifyContent="flex-start"
+    position="relative"
+    marginTop="20px"
+    paddingBottom="20px"
+    height="300px"
+    overflow="scroll"
+    className={styles.container}
+  >
+    <Text fontSize={18} fontWeight="bold">我的报酬</Text>
+    <Box justifyContent="center" margin="20px 0" width="100%">
+      {
+        data.map((it, index) => {
+          return (
+          <Flex key={`ex_${index}`} justifyContent="flex-start" gap="30px" background="rgba(255,255,255,0.05)" borderRadius={4} margin="10px 0" padding="30px 25px">
+            <Flex justifyContent="center" alignItems="center" width="60px" height="60px" borderRadius={4} background="#7551FF">
+              任务签约
+            </Flex>
+            <Flex direction="column" gap="10px">
+              <Box><Text>任务名称任务名称</Text></Box>
+              <Flex gap="10px"><Text>任务状态</Text> <Text>已签约</Text></Flex>
+            </Flex>
+            <Flex direction="column" gap="10px">
+              <Box><Text>600.00 USDT</Text></Box>
+              <Flex gap="10px"><Text>约合</Text> <Text>4380.00 CNY</Text></Flex>
+            </Flex>
+            <Flex direction="column" gap="10px">
+              <Box><Text>质押中</Text></Box>
+            </Flex>
+            <Flex direction="column" gap="10px">
+              <Box><Text>提到钱包</Text></Box>
+            </Flex>
+          </Flex>
+          )
+        })
+      }
+    </Box>
+</Box>
+}
