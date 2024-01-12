b47d3fa89706ab4ea7cf34a0d9a3c62541176bd8
leeweb3 <mw5130558@gmail.com>
2024-01-12 14:03:16 +0800

    feat: base url

--- src/common/utils/axios.ts
+++ src/common/utils/axios.ts
@@ -3,8 +3,7 @@ axios.defaults.timeout = 180000;
 import { parseJson } from './index';
 
 const axiosInstance: AxiosInstance = axios.create({
-  // baseURL: '/',
-  baseURL: 'http://42.192.37.117:8080/',
+  baseURL: '/',
   headers: {
     'Content-Type': 'application/json',
   },
