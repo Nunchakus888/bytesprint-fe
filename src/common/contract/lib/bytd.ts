eccddf770114ddd77e0c7e28c2e3d1650d0fcfa7
bella <bellawmm69@gmail.com>
2024-04-22 19:38:54 +0800

    feat: comment

--- src/common/contract/lib/bytd.ts
+++ src/common/contract/lib/bytd.ts
@@ -9,24 +9,6 @@ import BN from 'bignumber.js';
 const BYTD_ADDRESS = '0xb19C40e44B6a56Ef0C98F248B6AC31aE948CbaFf';
 const USDT_B_ADDRESS = '0xD8d76d720b0250207fDa709ad59aBa164099d323';
 
-const str = `Error: cannot estimate gas; transaction may fail or may require manual gas limit [ See: https://links.ethers.org/v5-errors-UNPREDICTABLE_GAS_LIMIT ] (reason="execution reverted: task status should be signed", method="estimateGas", transaction={"from":"0x25CE7C385300576d94f2620b7AB7B449be7cF4D9","to":"0xb19C40e44B6a56Ef0C98F248B6AC31aE948CbaFf","data":"0x5f944326000000000000000000000000000000000000000000000000000000000000000e","accessList":null}, error={"code":-32603,"message":"execution reverted: task status should be signed","data":{"originalError":{"code":3,"data":"0x08c379a00000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000001c7461736b207374617475732073686f756c64206265207369676e656400000000","message":"execution reverted: task status should be signed"}}}, code=UNPREDICTABLE_GAS_LIMIT, version=providers/5.7.2)
-at Logger.makeError (index.js:231:23)
-at Logger.throwError (index.js:240:20)
-at checkError (json-rpc-provider.js:92:20)
-at Web3Provider.eval (json-rpc-provider.js:591:24)
-at Generator.throw (<anonymous>)
-at rejected (json-rpc-provider.js:22:65)`;
-
-// const errorInfo =
-//   '0xba2d6ef800000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000008696e7075743d3d32000000000000000000000000000000000000000000000000';
-// function decodeErrorData(data: any, abi: any) {
-//   const iface = new ethers.utils.Interface(abi);
-//   debugger;
-//   const decoded = iface.decodeErrorResult('NoPermission', data);
-//   console.log('Decoded Data:', decoded);
-//   return decoded;
-// }
-
 //获取USDT实例
 const getUSDTInstance = async (address: string, readOnly = false) => {
   if (readOnly) {
@@ -170,12 +152,10 @@ export const startTask = async ({ projectId }: any) => {
     return false;
   } catch (e: any) {
     console.log(e.message);
-    debugger;
     // 检测是否已经存在，若存在，继续流程
     try {
       const iface = new ethers.utils.Interface(BYTD_ABI);
       const data = getCustomCodeError(e.message);
-      debugger;
       const decoded = iface.decodeErrorResult('TaskAlreadyInProgress', data);
       console.log(decoded);
       return true;
@@ -289,16 +269,6 @@ function getErrorMessage(message: string) {
   return null;
 }
 
-// evaluteTask Error
-/*
-Error: cannot estimate gas; transaction may fail or may require manual gas limit [ See: https://links.ethers.org/v5-errors-UNPREDICTABLE_GAS_LIMIT ] (reason="execution reverted", method="estimateGas", transaction={"from":"0x25CE7C385300576d94f2620b7AB7B449be7cF4D9","to":"0xb19C40e44B6a56Ef0C98F248B6AC31aE948CbaFf","data":"0xe630911d00000000000000000000000000000000000000000000000000000000000000190000000000000000000000000000000000000000000000000000000000000000","accessList":null}, error={"code":-32603,"message":"execution reverted","data":{"originalError":{"code":3,"data":"0x6236b1d100000000000000000000000025ce7c385300576d94f2620b7ab7b449be7cf4d90000000000000000000000000000000000000000000000000000000000000019","message":"execution reverted"}}}, code=UNPREDICTABLE_GAS_LIMIT, version=providers/5.7.2)
-at Logger.makeError (index.js:231:23)
-at Logger.throwError (index.js:240:20)
-at checkError (json-rpc-provider.js:92:20)
-at Web3Provider.eval (json-rpc-provider.js:591:24)
-at Generator.throw (<anonymous>)
-at rejected (json-rpc-provider.js:22:65)
-*/
 // 自定义错误
 function getCustomCodeError(message: string) {
   let error = '';
@@ -310,19 +280,3 @@ function getCustomCodeError(message: string) {
   }
   return null;
 }
-
-// function getTransactionErrorMessage(message: string) {
-//   // const d = `${message}`.match(/\"transaction=\"(.*)"/);
-//   const start = message.indexOf('transaction=');
-//   const end = message.indexOf('}');
-//   debugger;
-//   console.log(start + 12, end);
-//   const err = message.slice(start + 12, end + 1);
-//   console.log(err);
-//   debugger;
-//   if (err) {
-//     // @ts-ignore
-//     return JSON.parse(err).data;
-//   }
-//   return null;
-// }
